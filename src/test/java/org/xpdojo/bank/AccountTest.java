889a480ee8705fc8801d305832a08f5f877d156e
wiuppet-boy <john-r.grant@virginmedia.com>
2020-07-16 15:21:17 +0100

    Added negative withdraw test

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -27,7 +27,8 @@ public class AccountTest {
         assertThat(account.balance()).isEqualTo(-5);
     }
 
-    public void trasferAmount () {
+    @Test
+    public void transferAmount () {
         Account account = new Account();
         account.deposit(20);
         Account targetAccount = new Account();
@@ -37,8 +38,21 @@ public class AccountTest {
 
         assertThat(account.balance()).isEqualTo(15);
         assertThat(targetAccount.balance()).isEqualTo(10);
-
     }
+
+    @Test
+    public void transferAmountFail () {
+        Account account = new Account();
+        account.deposit(20);
+        Account targetAccount = new Account();
+        targetAccount.deposit(5);
+
+        account.transfer(targetAccount, 30);
+
+        assertThat(account.balance()).isEqualTo(20);
+        assertThat(targetAccount.balance()).isEqualTo(5);
+    }
+
 
 
 }
